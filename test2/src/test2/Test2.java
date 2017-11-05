ec3535cffe11c0301e859340eb1a98f8d2dfe815
akitakun <tantan_k@zenno.biz>
2017-11-05 23:07:32 +0900

    modify

--- test2/src/test2/Test2.java
+++ test2/src/test2/Test2.java
@@ -3,8 +3,9 @@ package test2;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+
 		System.out.println("test2");
+		System.out.println("test2-2");
 	}
 
 }
